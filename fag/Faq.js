24a7ff40abd084d3cf8c83234c18161abc92f1d9
Togrul302007 <tahirtogrul30@gmail.com>
2025-07-22 21:40:05 +0400

    Add files via upload

---
+++ fag/Faq.js
@@ -0,0 +1,488 @@
+function cavab1(){
+
+    let element = document.querySelector(".cavab1");
+    element.style.display = "block"
+    let login = document.querySelector(".mainlogin");
+    login.classList.add("blur")
+}
+
+
+function reverse(){
+    let elementim = document.querySelector(".mainlogin");
+    // let elementimm = document.querySelector(".cavab1");
+    // elementimm.classList.toggle("elave");
+    let sil = document.querySelector(".cavab1")
+    sil.style.display = "none"
+    elementim.classList.remove("blur");
+}
+
+function cavab2(){
+
+    let element1 = document.querySelector(".cavab2");
+    element1.style.display = "block"
+    let login1 = document.querySelector(".mainlogin");
+    login1.classList.add("blur")
+}
+
+
+function reverse1(){
+    let elementim2 = document.querySelector(".mainlogin");
+    // let elementimm = document.querySelector(".cavab1");
+    // elementimm.classList.toggle("elave");
+    let sil2 = document.querySelector(".cavab2")
+    sil2.style.display = "none"
+    elementim2.classList.remove("blur");
+}
+
+
+function cavab3(){
+
+    let element1 = document.querySelector(".cavab3");
+    element1.style.display = "block"
+    let login1 = document.querySelector(".mainlogin");
+    login1.classList.add("blur")
+}
+
+
+function reverse2(){
+    let elementim2 = document.querySelector(".mainlogin");
+    // let elementimm = document.querySelector(".cavab1");
+    // elementimm.classList.toggle("elave");
+    let sil2 = document.querySelector(".cavab3")
+    sil2.style.display = "none"
+    elementim2.classList.remove("blur");
+}
+
+function cavab4(){
+
+    let element1 = document.querySelector(".cavab4");
+    element1.style.display = "block"
+    let login1 = document.querySelector(".mainlogin");
+    login1.classList.add("blur")
+}
+
+
+function reverse3(){
+    let elementim2 = document.querySelector(".mainlogin");
+    // let elementimm = document.querySelector(".cavab1");
+    // elementimm.classList.toggle("elave");
+    let sil2 = document.querySelector(".cavab4")
+    sil2.style.display = "none"
+    elementim2.classList.remove("blur");
+}
+
+
+function cavab5(){
+
+    let element1 = document.querySelector(".cavab5");
+    element1.style.display = "block"
+    let login1 = document.querySelector(".mainlogin");
+    login1.classList.add("blur")
+}
+
+
+function reverse4(){
+    let elementim2 = document.querySelector(".mainlogin");
+    // let elementimm = document.querySelector(".cavab1");
+    // elementimm.classList.toggle("elave");
+    let sil2 = document.querySelector(".cavab5")
+    sil2.style.display = "none"
+    elementim2.classList.remove("blur");
+}
+
+
+function cavab6(){
+
+    let element1 = document.querySelector(".cavab6");
+    element1.style.display = "block"
+    let login1 = document.querySelector(".mainlogin");
+    login1.classList.add("blur")
+}
+
+
+function reverse5(){
+    let elementim2 = document.querySelector(".mainlogin");
+    // let elementimm = document.querySelector(".cavab1");
+    // elementimm.classList.toggle("elave");
+    let sil2 = document.querySelector(".cavab6")
+    sil2.style.display = "none"
+    elementim2.classList.remove("blur");
+}
+
+function cavab7(){
+
+    let element1 = document.querySelector(".cavab7");
+    element1.style.display = "block"
+    let login1 = document.querySelector(".mainlogin");
+    login1.classList.add("blur")
+}
+
+
+function reverse6(){
+    let elementim2 = document.querySelector(".mainlogin");
+    // let elementimm = document.querySelector(".cavab1");
+    // elementimm.classList.toggle("elave");
+    let sil2 = document.querySelector(".cavab7")
+    sil2.style.display = "none"
+    elementim2.classList.remove("blur");
+}
+
+function cavab8(){
+
+    let element1 = document.querySelector(".cavab8");
+    element1.style.display = "block"
+    let login1 = document.querySelector(".mainlogin");
+    login1.classList.add("blur")
+}
+
+
+function reverse7(){
+    let elementim2 = document.querySelector(".mainlogin");
+    // let elementimm = document.querySelector(".cavab1");
+    // elementimm.classList.toggle("elave");
+    let sil2 = document.querySelector(".cavab8")
+    sil2.style.display = "none"
+    elementim2.classList.remove("blur");
+}
+
+
+function cavab9(){
+
+    let element1 = document.querySelector(".cavab9");
+    element1.style.display = "block"
+    let login1 = document.querySelector(".mainlogin");
+    login1.classList.add("blur")
+}
+
+
+function reverse8(){
+    let elementim2 = document.querySelector(".mainlogin");
+    // let elementimm = document.querySelector(".cavab1");
+    // elementimm.classList.toggle("elave");
+    let sil2 = document.querySelector(".cavab9")
+    sil2.style.display = "none"
+    elementim2.classList.remove("blur");
+}
+
+
+function cavab10(){
+
+    let element1 = document.querySelector(".cavab10");
+    element1.style.display = "block"
+    let login1 = document.querySelector(".mainlogin");
+    login1.classList.add("blur")
+}
+
+
+function reverse9(){
+    let elementim2 = document.querySelector(".mainlogin");
+    // let elementimm = document.querySelector(".cavab1");
+    // elementimm.classList.toggle("elave");
+    let sil2 = document.querySelector(".cavab10")
+    sil2.style.display = "none"
+    elementim2.classList.remove("blur");
+}
+
+
+function cavab11(){
+
+    let element1 = document.querySelector(".cavab11");
+    element1.style.display = "block"
+    let login1 = document.querySelector(".mainlogin");
+    login1.classList.add("blur")
+}
+
+
+function reverse10(){
+    let elementim2 = document.querySelector(".mainlogin");
+    // let elementimm = document.querySelector(".cavab1");
+    // elementimm.classList.toggle("elave");
+    let sil2 = document.querySelector(".cavab11")
+    sil2.style.display = "none"
+    elementim2.classList.remove("blur");
+}
+
+
+function cavab12(){
+
+    let element1 = document.querySelector(".cavab12");
+    element1.style.display = "block"
+    let login1 = document.querySelector(".mainlogin");
+    login1.classList.add("blur")
+}
+
+
+function reverse11(){
+    let elementim2 = document.querySelector(".mainlogin");
+    // let elementimm = document.querySelector(".cavab1");
+    // elementimm.classList.toggle("elave");
+    let sil2 = document.querySelector(".cavab12")
+    sil2.style.display = "none"
+    elementim2.classList.remove("blur");
+}
+
+
+function cavab13(){
+
+    let element1 = document.querySelector(".cavab13");
+    element1.style.display = "block"
+    let login1 = document.querySelector(".mainlogin");
+    login1.classList.add("blur")
+}
+
+
+function reverse12(){
+    let elementim2 = document.querySelector(".mainlogin");
+    // let elementimm = document.querySelector(".cavab1");
+    // elementimm.classList.toggle("elave");
+    let sil2 = document.querySelector(".cavab13")
+    sil2.style.display = "none"
+    elementim2.classList.remove("blur");
+}
+
+
+function cavab14(){
+
+    let element1 = document.querySelector(".cavab14");
+    element1.style.display = "block"
+    let login1 = document.querySelector(".mainlogin");
+    login1.classList.add("blur")
+}
+
+
+function reverse13(){
+    let elementim2 = document.querySelector(".mainlogin");
+    // let elementimm = document.querySelector(".cavab1");
+    // elementimm.classList.toggle("elave");
+    let sil2 = document.querySelector(".cavab14")
+    sil2.style.display = "none"
+    elementim2.classList.remove("blur");
+}
+
+
+function cavab15(){
+
+    let element1 = document.querySelector(".cavab15");
+    element1.style.display = "block"
+    let login1 = document.querySelector(".mainlogin");
+    login1.classList.add("blur")
+}
+
+
+function reverse14(){
+    let elementim2 = document.querySelector(".mainlogin");
+    // let elementimm = document.querySelector(".cavab1");
+    // elementimm.classList.toggle("elave");
+    let sil2 = document.querySelector(".cavab15")
+    sil2.style.display = "none"
+    elementim2.classList.remove("blur");
+}
+
+
+function cavab16(){
+
+    let element1 = document.querySelector(".cavab16");
+    element1.style.display = "block"
+    let login1 = document.querySelector(".mainlogin");
+    login1.classList.add("blur")
+}
+
+
+function reverse15(){
+    let elementim2 = document.querySelector(".mainlogin");
+    // let elementimm = document.querySelector(".cavab1");
+    // elementimm.classList.toggle("elave");
+    let sil2 = document.querySelector(".cavab16")
+    sil2.style.display = "none"
+    elementim2.classList.remove("blur");
+}
+
+
+function cavab17(){
+
+    let element1 = document.querySelector(".cavab17");
+    element1.style.display = "block"
+    let login1 = document.querySelector(".mainlogin");
+    login1.classList.add("blur")
+}
+
+
+function reverse16(){
+    let elementim2 = document.querySelector(".mainlogin");
+    // let elementimm = document.querySelector(".cavab1");
+    // elementimm.classList.toggle("elave");
+    let sil2 = document.querySelector(".cavab17")
+    sil2.style.display = "none"
+    elementim2.classList.remove("blur");
+}
+
+
+function cavab18(){
+
+    let element1 = document.querySelector(".cavab18");
+    element1.style.display = "block"
+    let login1 = document.querySelector(".mainlogin");
+    login1.classList.add("blur")
+}
+
+
+function reverse17(){
+    let elementim2 = document.querySelector(".mainlogin");
+    // let elementimm = document.querySelector(".cavab1");
+    // elementimm.classList.toggle("elave");
+    let sil2 = document.querySelector(".cavab18")
+    sil2.style.display = "none"
+    elementim2.classList.remove("blur");
+}
+
+
+function cavab19(){
+
+    let element1 = document.querySelector(".cavab19");
+    element1.style.display = "block"
+    let login1 = document.querySelector(".mainlogin");
+    login1.classList.add("blur")
+}
+
+
+function reverse18(){
+    let elementim2 = document.querySelector(".mainlogin");
+    // let elementimm = document.querySelector(".cavab1");
+    // elementimm.classList.toggle("elave");
+    let sil2 = document.querySelector(".cavab19")
+    sil2.style.display = "none"
+    elementim2.classList.remove("blur");
+}
+
+
+function cavab20(){
+
+    let element1 = document.querySelector(".cavab20");
+    element1.style.display = "block"
+    let login1 = document.querySelector(".mainlogin");
+    login1.classList.add("blur")
+}
+
+
+function reverse19(){
+    let elementim2 = document.querySelector(".mainlogin");
+    // let elementimm = document.querySelector(".cavab1");
+    // elementimm.classList.toggle("elave");
+    let sil2 = document.querySelector(".cavab20")
+    sil2.style.display = "none"
+    elementim2.classList.remove("blur");
+}
+
+
+function cavab21(){
+
+    let element1 = document.querySelector(".cavab21");
+    element1.style.display = "block"
+    let login1 = document.querySelector(".mainlogin");
+    login1.classList.add("blur")
+}
+
+
+function reverse20(){
+    let elementim2 = document.querySelector(".mainlogin");
+    // let elementimm = document.querySelector(".cavab1");
+    // elementimm.classList.toggle("elave");
+    let sil2 = document.querySelector(".cavab21")
+    sil2.style.display = "none"
+    elementim2.classList.remove("blur");
+}
+
+
+function cavab22(){
+
+    let element1 = document.querySelector(".cavab22");
+    element1.style.display = "block"
+    let login1 = document.querySelector(".mainlogin");
+    login1.classList.add("blur")
+}
+
+
+function reverse21(){
+    let elementim2 = document.querySelector(".mainlogin");
+    // let elementimm = document.querySelector(".cavab1");
+    // elementimm.classList.toggle("elave");
+    let sil2 = document.querySelector(".cavab22")
+    sil2.style.display = "none"
+    elementim2.classList.remove("blur");
+}
+
+
+function cavab23(){
+
+    let element1 = document.querySelector(".cavab23");
+    element1.style.display = "block"
+    let login1 = document.querySelector(".mainlogin");
+    login1.classList.add("blur")
+}
+
+
+function reverse22(){
+    let elementim2 = document.querySelector(".mainlogin");
+    // let elementimm = document.querySelector(".cavab1");
+    // elementimm.classList.toggle("elave");
+    let sil2 = document.querySelector(".cavab23")
+    sil2.style.display = "none"
+    elementim2.classList.remove("blur");
+}
+
+
+function cavab24(){
+
+    let element1 = document.querySelector(".cavab24");
+    element1.style.display = "block"
+    let login1 = document.querySelector(".mainlogin");
+    login1.classList.add("blur")
+}
+
+
+function reverse23(){
+    let elementim2 = document.querySelector(".mainlogin");
+    // let elementimm = document.querySelector(".cavab1");
+    // elementimm.classList.toggle("elave");
+    let sil2 = document.querySelector(".cavab24")
+    sil2.style.display = "none"
+    elementim2.classList.remove("blur");
+}
+
+
+function cavab25(){
+
+    let element1 = document.querySelector(".cavab25");
+    element1.style.display = "block"
+    let login1 = document.querySelector(".mainlogin");
+    login1.classList.add("blur")
+}
+
+
+function reverse24(){
+    let elementim2 = document.querySelector(".mainlogin");
+    // let elementimm = document.querySelector(".cavab1");
+    // elementimm.classList.toggle("elave");
+    let sil2 = document.querySelector(".cavab25")
+    sil2.style.display = "none"
+    elementim2.classList.remove("blur");
+}
+
+
+function cavab26(){
+
+    let element1 = document.querySelector(".cavab26");
+    element1.style.display = "block"
+    let login1 = document.querySelector(".mainlogin");
+    login1.classList.add("blur")
+}
+
+
+function reverse25(){
+    let elementim2 = document.querySelector(".mainlogin");
+    // let elementimm = document.querySelector(".cavab1");
+    // elementimm.classList.toggle("elave");
+    let sil2 = document.querySelector(".cavab26")
+    sil2.style.display = "none"
+    elementim2.classList.remove("blur");
+}
